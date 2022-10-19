b0af9e84dacafb0c7227fa90b27f27d8f3db8666
RIP-Network <93273837+RIP-Network@users.noreply.github.com>
2022-10-19 19:29:18 +0200

    Update Termux.py

--- tools/Termux.py
+++ tools/Termux.py
@@ -13,6 +13,7 @@ calipso= '\033[36m'
 blanco = '\033[37m'
 cierre = '\033[39m'
 
+os.system('clear')
 print(Fore.GREEN + "              RIP-Network")
 print("                 V6.0")
 time.sleep(3)
@@ -21,12 +22,12 @@ time.sleep(3)
 def menu():
     while True:
      print(Fore.GREEN + "Opciones\n")     
-     print("1) Spam-SMS (Linux)                     RIP-Network                             ")                    
-     print("2) Instalar requisitos                  Version 6.0                             ")
-     print("3) Version de la herramienta ")
-     print("4) Spam-SMS (Termux)")
-     print("5) Como usar ")
-     print("99)Salir")
+     print("1) 1                  RIP-Network                             ")                    
+     print("2) 2                  Version 6.0                             ")
+     print("3) 3")
+     print("4) 4")
+     print("5) 5")
+     print("6) 6")
 
      d = input(Fore.LIGHTBLUE_EX + "Seleccione todas las opciones de 1 en 1 > ")
 
@@ -59,7 +60,7 @@ def menu():
         os.system("termux-open-url https://twitter.com/i/flow/password_reset?input_flow_data=%7B%22requested_variant%22%3A%22eyJwbGF0Zm9ybSI6IlJ3ZWIifQ%3D%3D%22%7D")
         
       
-     elif d == "99":
+     elif d == "6":
         os.system('clear')
         os.system("termux-open-url https://web.telegram.org/k/")
 
