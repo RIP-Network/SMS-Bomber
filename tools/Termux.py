3c5f9fad4ac5558dfc35a7b9aba211d2837492fd
RIP-Network <93273837+RIP-Network@users.noreply.github.com>
2022-10-19 19:26:35 +0200

    Update Termux.py

--- tools/Termux.py
+++ tools/Termux.py
@@ -1,4 +1,18 @@
-print(banner)
+import sys, os, webbrowser, platform, subprocess, time
+from colorama import Fore
+from importlib_metadata import version
+
+#colores
+negro = '\033[30m'
+rojo = '\033[31m'
+verde = '\033[32m'
+amarillo = '\033[33m'
+azul = '\033[34m'
+rosado = '\033[35m'
+calipso= '\033[36m'
+blanco = '\033[37m'
+cierre = '\033[39m'
+
 print(Fore.GREEN + "              RIP-Network")
 print("                 V6.0")
 time.sleep(3)
