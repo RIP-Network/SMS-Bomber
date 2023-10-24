94690ca954ff929c944a2588ec074a6597d6786c
RIP-Network <93273837+RIP-Network@users.noreply.github.com>
2023-10-24 19:41:15 +0200

    Update target.sh

--- tools/tools/target.sh
+++ tools/tools/target.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
-#
-# target: (26/01/2021)
-#
-# [Open Source] - [Código Abierto]
-#
-# Variables y Colores
-#
+
 PWD=$(pwd)
 OS=$(uname -o)
 USER=$(id -u)
@@ -19,9 +13,7 @@ amarillo='\033[33m'
 morado='\033[35m'
 cian='\033[1;36m'
 magenta='\033[1;35m'
-#
-# Mensaje de Opción Incorrecta
-#
+
 function Error {
 echo -e "${rojo}
 ┌═════════════════════┐
@@ -30,9 +22,7 @@ echo -e "${rojo}
 "${blanco}
 sleep 0.5
 }
-#
-# Banner SETSMS
-#
+
 function SETSMS {
 	sleep 0.5
 	clear
@@ -44,9 +34,7 @@ echo -e "${verde}
 ███████║███████╗   ██║   ███████║██║ ╚═╝ ██║███████║
 ╚══════╝╚══════╝   ╚═╝   ╚══════╝╚═╝     ╚═╝╚══════╝"${blanco}
 }
-#
-# Solicitando Número Telefónico
-#
+
 function PhoneNumber {
 SETSMS
 echo -e -n "${verde}
@@ -93,9 +81,7 @@ echo -e "${verde}
 "${blanco}
 read
 }
-#
-# Llamando a las Herramientas Quack e Impulse
-#
+
 function SendSMS {
 cd ${RUTA}/quack
 python3 quack --tool SMS --target ${PHONE} --threads 60 --timeout 90
@@ -168,9 +154,7 @@ Error
 Backup
 fi
 }
-#
-# Regresando al Menu Principal
-#
+
 function Restart {
 echo -e "${verde}
 ┌════════════════════════════┐
@@ -179,9 +163,7 @@ echo -e "${verde}
 read
 source ${RUTA}/SETSMS.sh
 }
-#
-# Declarando Funciones
-#
+
 PhoneNumber
 SendSMS
 Backup
